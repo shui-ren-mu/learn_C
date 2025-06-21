b8358233c20e1a7dfd853560baece9c61b5404bd
shui-ren-mu <LCY-diamond@Outlook.com>
2025-06-21 14:32:57 +0800

    头文件介绍.cpp
    介绍了头文件。

---
+++ 头文件介绍.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>	//万能头文件 (以后都将以这个做示范) (1)
+#include<iostream>      //用于基本的输入输出操作(2)
+#include<string>        //用于处理字符串(3)
+#include<algorithm>     //包含了很多常用的算法(4)
+#include<vector>
+#include<list>
+#include<map>           //提供了各种数据结构。(5-7)
+using namespace std;
+int main(){
+	    //代码 
+}
